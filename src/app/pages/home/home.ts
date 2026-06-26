304f13c4f4d9c544ff2a577ef3c60a106939d5de
AVANI BIJU <avanibiju26@gmail.com>
2026-06-26 12:43:32 +0530

    git commit -m "Add ecommerce UI, routing and product pages"

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Carousel } from "../../components/carousel/carousel";
 import { Accordion } from "../../components/accordion/accordion";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [Carousel, Accordion],
+  imports: [Carousel, Accordion,RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
